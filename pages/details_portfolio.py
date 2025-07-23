4477a171fb7e587524c9a2750ff0a0442c27e65c
krishnagupta-hub1 <162449103+krishnagupta-hub1@users.noreply.github.com>
2025-07-24 02:45:22 +0530

    Create details_portfolio.py

---
+++ pages/details_portfolio.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+def draw():
+    st.title("📁 Details and Portfolio")
+    st.markdown("### Connect with Me")
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.image("https://img.icons8.com/ios-filled/100/ffffff/instagram-new.png", width=80)
+        if st.button("Instagram"):
+            st.markdown("[Click here](https://www.instagram.com/krishnagupta___07/?__pwa=1)", unsafe_allow_html=True)
+    with col2:
+        st.image("https://img.icons8.com/ios-filled/100/ffffff/github.png", width=80)
+        if st.button("GitHub"):
+            st.markdown("[Click here](https://github.com/krishnagupta-hub1)", unsafe_allow_html=True)
+    with col3:
+        st.image("https://img.icons8.com/ios-filled/100/ffffff/linkedin.png", width=80)
+        if st.button("LinkedIn"):
+            st.markdown("[Click here](https://www.linkedin.com/in/krishna-gupta-63b354216)", unsafe_allow_html=True)
+    with col4:
+        st.image("https://img.icons8.com/ios-filled/100/ffffff/twitterx.png", width=80)
+        if st.button("Twitter (X)"):
+            st.markdown("[Click here](https://x.com/KrishnaGup72761)", unsafe_allow_html=True)
