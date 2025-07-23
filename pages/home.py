a963e2c590670dd75bd7847446365440207d1506
krishnagupta-hub1 <162449103+krishnagupta-hub1@users.noreply.github.com>
2025-07-24 02:39:51 +0530

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -1,5 +1,17 @@
 import streamlit as st
+from core.date_utils import get_display_date
 
-def render():
+def draw():
     st.title("🏠 Welcome to Your Daily App")
-    st.markdown("This is your personalized productivity and tracking dashboard.")
+    for i in range(1, 7):
+        st.markdown(f"<div style='background-color:#d0ebff;padding:15px;border-radius:10px;margin-top:10px;'>🔹 Section {i}</div>", unsafe_allow_html=True)
+    st.markdown("""<hr style='margin-top:30px;margin-bottom:10px;border:1px solid #ccc;'>""", unsafe_allow_html=True)
+    st.markdown("""
+        <div style='font-size:16px;color:#888;'>
+            <strong>#At Night</strong><br>
+            Update dairy / Twitter / Git<br>
+            Check mails / LinkedIn / Organisation / Instagram<br>
+            (search / commitChanges)<br>
+            - &nbsp;&nbsp;&nbsp; - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -
+        </div>
+    """, unsafe_allow_html=True)
